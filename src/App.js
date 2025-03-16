f317c89ccee67f1a98493d320bed8f49ad4708d1
chithirai-selvan <sakthisiddhu14@gmail.com>
2025-03-17 00:44:56 +0530

    Fix routing issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import SignUp from './components/SignUp';
 import AdminLandingPage from './components/AdminLandingPage';
@@ -12,11 +12,11 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path='/SignUp' element={<SignUp />} />
-        <Route path='/AdminLandingPage' element={<AdminLandingPage />} />
-        <Route path='/ParticipantLandingPage' element={<ParticipantLandingPage />} />
-        <Route path='/AddEvent' element={<AddEvent />} />
-        <Route path='/event/:eventId' element={<EventDetailsPage />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/AdminLandingPage" element={<AdminLandingPage />} />
+        <Route path="/ParticipantLandingPage" element={<ParticipantLandingPage />} />
+        <Route path="/AddEvent" element={<AddEvent />} />
+        <Route path="/event/:eventId" element={<EventDetailsPage />} />
       </Routes>
     </Router>
   );
